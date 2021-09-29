d6b60bfbe1184bad1603063da6232c4bc53119cb
babulakterfsd <babulakterfsd@gmail.com>
2021-09-29 23:09:28 +0600

    toastify added and multiple dev adding blocked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
-import Developerteam from './components/Developerteam/Developerteam';
-import Header from './components/Header/Header';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Developerteam from "./components/Developerteam/Developerteam";
+import Header from "./components/Header/Header";
+import { Flip } from "react-toastify";
 
 function App() {
   return (
     <div className="App pb-2">
       <Header></Header>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        rtl={false}
+        transition={Flip}
+      />
       <Developerteam></Developerteam>
     </div>
   );
--- src/components/Developerteam/Developerteam.js
+++ src/components/Developerteam/Developerteam.js
@@ -1,36 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import Developers from './Developers/Developers';
-import Cart from './Cart/Cart'
+import React, { useEffect, useState } from "react";
+import Developers from "./Developers/Developers";
+import Cart from "./Cart/Cart";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // developers and cart both component are rendered from here
 const Developerteam = () => {
-    const [developers, setDevelopers] = useState([]);
-    const [hiredDeveloper, setHiredDeveloper] = useState([]);
-   
-    //fetching json data from external resource
-    useEffect(() => {
-       fetch('./devlist.json')
-        .then(res => res.json())
-         .then(data => setDevelopers(data))
-    }, [])
-   
-    //handling clik event on each devloper hire button
-   const handleHire = developer => {
-     setHiredDeveloper([...hiredDeveloper, developer])
-   }
+  const [developers, setDevelopers] = useState([]);
+  const [hiredDeveloper, setHiredDeveloper] = useState([]);
 
-    return (
-        <div className="container-fluid">
-          <div className="row">
-           <div className="col-12 col-lg-9 developersContainer">
-             <Developers developers={developers} handleHire={handleHire}></Developers>
-           </div>
-           <div className="col-10 col-md-6 mx-auto col-lg-3 mt-5 mt-lg-0 cartContainer">
-               <Cart hiredDeveloper={hiredDeveloper}></Cart>
-           </div>
-          </div>
+  //fetching json data from external resource
+  useEffect(() => {
+    fetch("./devlist.json")
+      .then((res) => res.json())
+      .then((data) => setDevelopers(data));
+  }, []);
+
+  const notify = () => toast("Wow! Developer Hired!", { theme: "dark" });
+  const notifyError = () => toast.error("This Developer Is Already Hired!");
+  const handleHire = (developer) => {
+    setHiredDeveloper([...new Set([...hiredDeveloper, developer])]);
+    //toastify
+    for (const object of hiredDeveloper) {
+      if (object.id === developer.id) {
+        notifyError();
+        return;
+      }
+    }
+    notify();
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 col-lg-9 developersContainer">
+          <Developers
+            developers={developers}
+            handleHire={handleHire}
+          ></Developers>
         </div>
-    );
+        <div className="col-10 col-md-6 mx-auto col-lg-3 mt-5 mt-lg-0 cartContainer">
+          <Cart hiredDeveloper={hiredDeveloper}></Cart>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Developerteam;
