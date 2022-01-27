621aa03865230eea10e92ee591a9cc9d78088c06
Babul Akter <babulakterfsd@gmail.com>
2022-01-27 12:07:21 +0600

    Update Banner.js

--- src/components/Header/Banner/Banner.js
+++ src/components/Header/Banner/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
         <div className="container">
                   <div className="row">
                       <div className="col-12 col-md-10 mx-auto bg-muted text-secondary my-2 text-center ">
-                         <h1>LeechCoder: Hire world class Developers</h1>
+                         <h1>Developers: Hire world class Developers</h1>
                          <h5>We are a stunning team to boostup your online business</h5>
                          <h3>Total Budget: $100 Billion</h3>
                       </div>
